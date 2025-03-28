ff5e89918113344940463a214c32f9c6e3284ca6
liandd <juandavidtv24@gmail.com>
2025-03-28 09:52:22 -0500

    beecrowd judge

---
+++ BeeCrowd/1020.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string n=0;
+    cin>>n;
+    int a=0,m=0,d=0;
+    if((stoi(n))>365){
+        a++;
+        m=stoi(n)-365;
+        if(m<365){
+
+        }
+    }
+
+return 0;
+}
