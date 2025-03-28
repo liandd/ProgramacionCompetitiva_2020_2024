2b2388e7faac9c4bc04f7d6de179c5eda8b81f9c
liandd <juandavidtv24@gmail.com>
2025-03-28 09:52:52 -0500

    beecrowd judge

---
+++ BeeCrowd/1026.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>          
+using namespace std;             
+                                 
+int main(){                      
+ unsigned int x=0, y=0, suma=0;   
+ while(cin>>x>>y){
+  suma = x ^ y;                  
+  cout << suma <<"\n";           
+ }                               
+  return 0;                      
+}   
