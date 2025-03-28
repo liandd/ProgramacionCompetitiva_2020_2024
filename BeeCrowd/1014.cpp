fca6334de3d4f66d28c5a8b5b0db00a694fd8925
liandd <juandavidtv24@gmail.com>
2025-03-28 09:50:27 -0500

    beecrowd judge

---
+++ BeeCrowd/1014.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double x=0,y=0,comp=0;
+    cin>>x>>y;
+    comp=x/y;
+    cout.setf(ios::fixed);
+    cout.precision(3);
+    cout<<comp<<" km/l\n";
+
+    return 0;
+}
